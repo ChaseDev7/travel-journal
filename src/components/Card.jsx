60d3f8e3a81d200f6e330ac4a8f1fa8cd468380a
ChaseDev7 <chasewebdev101@gmail.com>
2023-12-08 02:29:11 -0800

    Add all locations to card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,8 +5,14 @@ export default function Card(props) {
     <div className="card-container">
       <img src={`../../public/images/${props.cardData.travelImg}`} className="travel-image" />
       <div className="location-container">
-        <img src={mapIcon} className="map-icon" />
-        <p className="city-title">{props.cardData.city}</p>
+        <div className="city-container">
+          <img src={mapIcon} className="map-icon" />
+          <p className="city-title">{props.cardData.city}</p>
+          <p className="google-maps-link">View on Google Maps</p>
+        </div>
+        <h1 className="location-title">{props.cardData.location}</h1>
+        <h3 className="travel-date">{props.cardData.dates}</h3>
+        <p className="location-description">{props.cardData.description}</p>
       </div>
     </div>
   )
--- src/components/travel-data.jsx
+++ src/components/travel-data.jsx
@@ -3,8 +3,24 @@ export default [
     id: 1,
     travelImg: "cn-tower.jpg",
     city: "TORONTO",
-    url: "",
     location: "CN Tower",
-    dates: "15 Jan, 2021 - 24 Jan, 2021"
+    dates: "12 Jan, 2021 - 24 Jan, 2021",
+    description: "The CN Tower is a 553.3 m-high concrete communications and observation tower in Toronto, Ontario, Canada. It houses several observation decks, a revolving restaurant at some 350 metres, and an entertainment complex."
+  },
+  {
+    id: 2,
+    travelImg: "whistler.jpg",
+    city: "VANCOUVER",
+    location: "Whistler",
+    dates: "27 May, 2021 - 8 Jun, 2021",
+    description: "Whistler is a resort municipality in Squamish-Lillooet Regional District, British Columbia, Canada. Over two million people visit Whistler annually, primarily for alpine skiing and snowboarding and, in the summer, mountain biking at Whistler Blackcomb."
+  },
+  {
+    id: 3,
+    travelImg: "niagara-falls.jpg",
+    city: "NIAGARA FALLS",
+    location: "Niagara Falls",
+    dates: "01 Oct, 2021 - 18 Nov, 2021",
+    description: "Niagara Falls is a group of three waterfalls, spanning the border between Canada and the United States. Formed by the Niagara River, the combined falls have the highest flow rate of any waterfall in North America."
   }
 ]
